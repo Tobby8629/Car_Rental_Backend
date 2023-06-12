c96f509b194ce33fcbfa1def228a96b54cd6b6e7
CHRIS <NWAKAUC1@GMAIL.COM>
2023-06-12 12:15:13 +0200

    Reset Migrations

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_09_113330) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
