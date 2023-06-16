d20f702c28c51721727087edce117f079305cb11
CHRIS <NWAKAUC1@GMAIL.COM>
2023-06-16 19:10:48 +0200

    update

--- app/services/authentication_token_service.rb
+++ app/services/authentication_token_service.rb
@@ -3,7 +3,7 @@ class AuthenticationTokenService
   ALGORITHM_TYPE = 'HS256'.freeze
 
   def self.call(user_id)
-    payload = { user_id: }
+    payload = { user_id: user_id}
     JWT.encode payload, HMAC_SECRET, ALGORITHM_TYPE
   end
 
