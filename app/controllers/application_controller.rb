088e0158b97f3b0ce45e8024ebabf6681799c89a
CHRIS <NWAKAUC1@GMAIL.COM>
2023-06-16 20:31:54 +0200

    Implement current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,14 @@ class ApplicationController < ActionController::API
   end
 
   def current_user!
-    @current_user = User.find_by(id: payload[0]['user_id'])
+    if payload && AuthenticationTokenService.valid_payload(payload.first)
+      user_id = payload.first['user_id']
+      @current_user = User.find_by(id: user_id)
+    else
+      @current_user = User.new(id: -1) # Assign a default ID value, such as -1
+    end
   end
+  
 
   private
 
