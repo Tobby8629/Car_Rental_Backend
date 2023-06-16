82e4c21e931a462aa206d5ab41526d6b28d43f34
CHRIS <NWAKAUC1@GMAIL.COM>
2023-06-16 18:11:45 +0200

    update user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,17 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
 
   def authenticate_request!
-    return invalid_authentication if !payload || !AuthenticationTokenService.valid_payload(payload.first)
-
-    current_user!
+    return invalid_authentication unless current_user!
+  
     invalid_authentication unless @current_user
   end
+  
 
   def current_user!
-    @current_user = User.find_by(id: payload[0]['user_id'])
+    user_id = payload&.first&.dig('user_id')
+    @current_user = User.find_by(id: user_id) if user_id
   end
+  
 
   private
 
